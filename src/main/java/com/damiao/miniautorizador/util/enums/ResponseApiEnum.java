03d8a30b4c618494fb6235f86f1bed5d70a10acf
Damiao Leonardo <damiao.silva@sankhya.com.br>
2025-05-05 21:37:29 -0300

    feat: Ajuste no Enum ( configuracao dos valores)

--- src/main/java/com/damiao/miniautorizador/util/enums/ResponseApiEnum.java
+++ src/main/java/com/damiao/miniautorizador/util/enums/ResponseApiEnum.java
@@ -1,30 +1,18 @@
 package com.damiao.miniautorizador.util.enums;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-public enum RoleEnum {
-  ADMINISTRATOR(1L,"Administrador","ROLE_ADMINISTRATOR"),
+@AllArgsConstructor
+public enum ResponseApiEnum {
 
-  SUPPLIER(2L, "Fornecedor","ROLE_SUPPLIER"),
+  CARD_NOT_FOUND("Cartão não encontrado"),
+  INSUFFICIENT_BALANCE("Saldo insuficiente"),
+  INVALID_PASSWORD("Senha inválida"),
+  CARTAO_INEXISTENTE("Cartão inexistente"),
+  OK("OK");
 
-  BUYER(3L, "Comprador","ROLE_BUYER"),
+  private final String message;
 
-  SUPERVISOR(4L, "Fiscal","ROLE_SUPERVISOR"),
-
-  SECRETARY(5L, "Secretário","ROLE_SECRETARY"),
-
-  CONSULTANT(6L, "Consultor","ROLE_CONSULTANT");
-
-  private final Long id;
-
-  private final String name;
-
-  private final String description;
-
-  RoleEnum(Long id, String name, String description) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-  }
 }
