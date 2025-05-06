0a3e7f64eae0455d1f978a9add8e6afbe90c9dc0
Damiao Leonardo <damiao.silva@sankhya.com.br>
2025-05-06 11:55:45 -0300

    feat: Habilitando o Retry no Spring

--- src/main/java/com/damiao/miniautorizador/MiniautorizadorApplication.java
+++ src/main/java/com/damiao/miniautorizador/MiniautorizadorApplication.java
@@ -2,8 +2,10 @@ package com.damiao.miniautorizador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class MiniautorizadorApplication {
 
 	public static void main(String[] args) {
